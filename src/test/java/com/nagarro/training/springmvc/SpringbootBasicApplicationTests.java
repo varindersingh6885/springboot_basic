31949da1b6261531e8f6300de3e5dcf7d961ef9b
Varinder Singh <varinder.singh01@nagarro.com>
2022-12-26 14:08:27 +0530

    basic spring boot app

---
+++ src/test/java/com/nagarro/training/springmvc/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.training.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
